b6e8cf1e446ff18f27150213fca9061edcda6bc5
rehne <rene.honnen@smail.th-koeln.de>
2016-04-26 15:26:10 +0200

    updated app.js

--- honnen/app.js
+++ honnen/app.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 
 fs.readFile('../wolkenkratzer.json', function(err, data){
-  if(err) throw err;
-  var bigString = JSON.stringify(data.toString());
-  var array = bigString.split("\\n");
-  console.log(array);
+  if(err){
+    console.log('Error: ' + err);
+    return;
+  }
+  var string = JSON.parse(data);
+  console.log(string);
 });
