6ad2a84facf511a8aefab72e756b86bacae45d23
rehne <rene.honnen@smail.th-koeln.de>
2016-04-26 16:18:56 +0200

    Aufgabe 1

--- honnen/app.js
+++ honnen/app.js
@@ -5,6 +5,12 @@ fs.readFile('../wolkenkratzer.json', function(err, data){
     console.log('Error: ' + err);
     return;
   }
-  var string = JSON.parse(data);
-  console.log(string);
+  var objekt = JSON.parse(data);
+
+  for(var i = 0; i < objekt.wolkenkratzer.length; i++){
+    console.log('Name: '  + objekt.wolkenkratzer[i]['name']);
+    console.log('Stadt: ' + objekt.wolkenkratzer[i]['stadt']);
+    console.log('Höhe: '  + objekt.wolkenkratzer[i]['hoehe'] + 'm');
+    console.log('--------------------');
+  }
 });
