a27958463897ebc3410def0bd63a8624cf8a82bc
rehne <rene.honnen@smail.th-koeln.de>
2016-05-20 12:54:27 +0200

    ignore DS_Store files

--- moviefy/app.js
+++ moviefy/app.js
@@ -3,11 +3,26 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 var app = express();
+var serverPort = 3000;
 
 var movies = [
   {title: "12 Years A Slave"},
   {title: "Anchorman"}
 ]
+
+app.get('/', function(req, res){
+  var acceptedTypes = req.accepts(['html', 'json']);
+  switch (acceptedTypes) {
+    case 'html':
+      res.type('html').send('<h1>Hello World in HTML</h1>');
+      break;
+    case 'json':
+      res.json({content: 'Hello World in JSON'});
+      break;
+    default:
+      res.status(406).end();
+  }
+});
 
 app.get('/movies', function(req, res){
   res.status(200).json(movies);
@@ -18,4 +33,6 @@ app.post('/movies', jsonParser, function(req, res){
   res.type('plain').send('Added!');
 });
 
-app.listen(1337);
+app.listen(serverPort, function(){
+  console.log('Server listening on Port ' + serverPort + '.');
+});
